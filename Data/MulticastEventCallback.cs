d25c257e7d1b0d39f0cc5ffb6545114998d5cb6e
anakod <me@anakod.ru>
2020-12-27 02:36:41 +0500

    ohh..

---
+++ Data/MulticastEventCallback.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace WConfigurator.Data
+{
+    public class MulticastEventCallback
+    {
+        internal List<EventCallbackHandler> eventCallbacks = new List<EventCallbackHandler>();
+
+        public MulticastEventCallback()
+        {
+        }
+
+        internal MulticastEventCallback(List<EventCallbackHandler> list)
+        {
+            eventCallbacks = list;
+        }
+
+        public static MulticastEventCallback operator +(MulticastEventCallback left, Action callback)
+        {
+            var added = new EventCallbackHandler(callback);
+            return AddHandler(left, added);
+        }
+
+        public static MulticastEventCallback operator +(MulticastEventCallback left, Func<Task> callback)
+        {
+            var added = new EventCallbackHandler(callback);
+            return AddHandler(left, added);
+        }
+
+        public static MulticastEventCallback operator +(MulticastEventCallback left, IHandleEvent receiver)
+        {
+            var added = new EventCallbackHandler(receiver);
+            return AddHandler(left, added);
+        }
+
+        public static MulticastEventCallback operator -(MulticastEventCallback left, IHandleEvent receiver)
+        {
+            var list = new List<EventCallbackHandler>();
+            if (left != null)
+            {
+                var filtered = left.eventCallbacks.Where(x => x.Receiver != receiver);
+                list.AddRange(filtered);
+            }
+
+            return new MulticastEventCallback(list);
+        }
+
+        private static MulticastEventCallback AddHandler(MulticastEventCallback left, EventCallbackHandler added)
+        {
+            var list = new List<EventCallbackHandler>();
+            if (left != null)
+                list.AddRange(left.eventCallbacks);
+
+            if (!list.Select(x => x.Receiver).Contains(added.Receiver))
+                list.Add(added);
+
+            return new MulticastEventCallback(list);
+        }
+
+        public async Task InvokeAsync()
+        {
+            foreach (var it in eventCallbacks)
+            {
+                await it.InvokeAsync();
+            }
+        }
+    }
+
+    internal struct EventCallbackHandler
+    {
+        public EventCallbackHandler(MulticastDelegate callback)
+        {
+            Callback = callback;
+            Receiver = callback.Target as IHandleEvent;
+        }
+
+        public EventCallbackHandler(IHandleEvent handler)
+        {
+            Callback = null;
+            Receiver = handler;
+        }
+
+        public static IHandleEvent GetReceiver(EventCallback callback)
+        {
+            var prop = callback.GetType().GetField("Receiver", BindingFlags.NonPublic | BindingFlags.Instance);
+            return prop.GetValue(callback) as IHandleEvent;
+        }
+
+        public Task InvokeAsync()
+        {
+            if (HasDelegate)
+                return Receiver.HandleEventAsync(new EventCallbackWorkItem(Callback), null);
+            else if (Receiver != null)
+                return Receiver.HandleEventAsync(new EventCallbackWorkItem(), null);
+
+            return Task.CompletedTask;
+        }
+
+        public MulticastDelegate Callback { get; }
+        public IHandleEvent Receiver { get; }
+        public bool HasDelegate => Callback != null;
+    }
+}
---
+++ Data/MulticastSingleInstanceEventCallback.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WConfigurator.Data
+{
+    /// <summary>
+    /// Multicast EventCallback with single instance per target *type*. Previous references will be automatically removed when new added
+    /// </summary>
+    public class MulticastSingleInstanceEventCallback : MulticastEventCallback
+    {
+        public MulticastSingleInstanceEventCallback()
+        {
+        }
+        internal MulticastSingleInstanceEventCallback(List<EventCallbackHandler> list) : base(list)
+        {
+        }
+
+        public static MulticastSingleInstanceEventCallback operator +(MulticastSingleInstanceEventCallback left, Action callback)
+        {
+            var added = new EventCallbackHandler(callback);
+            return AddHandler(left, added);
+        }
+
+        public static MulticastSingleInstanceEventCallback operator +(MulticastSingleInstanceEventCallback left, Func<Task> callback)
+        {
+            var added = new EventCallbackHandler(callback);
+            return AddHandler(left, added);
+        }
+
+        public static MulticastSingleInstanceEventCallback operator +(MulticastSingleInstanceEventCallback left, IHandleEvent receiver)
+        {
+            var added = new EventCallbackHandler(receiver);
+            return AddHandler(left, added);
+        }
+
+        public static MulticastSingleInstanceEventCallback operator -(MulticastSingleInstanceEventCallback left, IHandleEvent receiver)
+        {
+            var list = new List<EventCallbackHandler>();
+            if (left != null)
+            {
+                var filtered = left.eventCallbacks.Where(x => x.Receiver != receiver);
+                list.AddRange(filtered);
+            }
+
+            return new MulticastSingleInstanceEventCallback(list);
+        }
+
+        private static MulticastSingleInstanceEventCallback AddHandler(MulticastSingleInstanceEventCallback left, EventCallbackHandler added)
+        {
+            var list = new List<EventCallbackHandler>();
+            if (left != null)
+            {
+                var actual = left.eventCallbacks.Where(x => x.Receiver.GetType() != added.Receiver.GetType());
+                list.AddRange(actual);
+            }
+
+            if (!list.Select(x => x.Receiver).Contains(added.Receiver))
+                list.Add(added);
+
+            return new MulticastSingleInstanceEventCallback(list);
+        }
+    }
+}
